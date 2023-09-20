6a1336d255f248e31ac731f2b61ee1c54de39899
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2023-09-20 11:24:24 -0300

    Finalizando a escolha dos minutos do temporizador

--- src/FocusTimer/eventos.js
+++ src/FocusTimer/eventos.js
@@ -5,6 +5,9 @@ import { controles } from "./elementos.js"
 import * as actions from './acoes.js'
 
 import * as elementos from './elementos.js'
+import estado from "./estado.js"
+
+import { updateDisplay } from "./timer.js"
 
 export function registroEventoControles(){
 
@@ -31,6 +34,22 @@ export function setMinutes() {
     elementos.minutos.onkeypress = (evento) => 
         //expresão regular tudo que nao for número retorna false
         /\d/.test(evento.key)
-    
+
+        // se passar de 60, quando sair do focus, fica somente 60
+        elementos.minutos.addEventListener('blur', (evento) => {
+            let time = evento.currentTarget.textContent
+
+
+            // operador ternário
+            time = time > 60 ? 60 : time
+
+
+            estado.minutos = time
+            estado.segundos = 0
+
+            updateDisplay()
+
+            elementos.minutos.removeAttribute('contenteditable')
+        })
 
 }   
